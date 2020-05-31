a9e0a00a5da8e9ddc3da06b20a96b884c9c23317
hershy245 <60234241+hershy245@users.noreply.github.com>
2020-05-31 14:03:10 -0400

    Updates

---
+++ precache-manifest.129766afd2fd8c6fd4f4a59597fa1851.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a9572b275581a3e0c06fdd38b9ed92c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bbd8c3544655ca82f981",
+    "url": "/static/css/main.95059b3e.chunk.css"
+  },
+  {
+    "revision": "660592e20f19f0b45b59",
+    "url": "/static/js/2.78990914.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.78990914.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbd8c3544655ca82f981",
+    "url": "/static/js/main.fdbc530c.chunk.js"
+  },
+  {
+    "revision": "4f270cfaca814e063cbd",
+    "url": "/static/js/runtime-main.bcf3ee24.js"
+  }
+]);
